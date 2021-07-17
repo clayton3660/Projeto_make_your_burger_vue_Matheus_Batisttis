d58e1dde865f46b19945134dcdb7d4f06d0b32db
Clayton Silva <clayton3660@gmail.com>
2021-07-17 12:10:08 -0300

    Aula32_Atualização_de_pedidos

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -25,10 +25,14 @@
           </ul>
         </div>
         <div>
-          <select name="status" class="status">
+          <select
+            name="status"
+            class="status"
+            @change="updatedBurger($event, burger.id)"
+          >
             <option value="">Selecione</option>
             <option
-              value="s.tipo"
+              :value="s.tipo"
               v-for="s in status"
               :key="s.id"
               :selected="burger.status == s.tipo"
@@ -78,6 +82,17 @@ export default {
       });
       const res = await req.json();
       this.getPedidos();
+    },
+    async updatedBurger(event, id) {
+      const option = event.target.value;
+      const dataJson = JSON.stringify({ status: option });
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "PATCH",
+        headers: { "Content-type": "application/json" },
+        body: dataJson,
+      });
+      const res = await req.json();
+      console.log(res);
     },
   },
   mounted() {
